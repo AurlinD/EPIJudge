ca5be82d9878910e4490caa6c71a818ace8c1ed9
Aurlin Dhillon <aurlindhillon@gmail.com>
2020-09-22 20:49:28 -0700

    brute force parity.java

--- epi_judge_java/epi/Parity.java
+++ epi_judge_java/epi/Parity.java
@@ -1,18 +1,26 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
 public class Parity {
   @EpiTest(testDataFile = "parity.tsv")
   public static short parity(long x) {
     // TODO - you fill in here.
-    return 0;
+    int acc = 0;
+    while (x != 0) {
+      acc += (x & 1);
+      x >>>= 1;
+    }
+    if (acc % 2 == 0) {
+      return 0;
+    } else {
+      return 1;
+    }
   }
 
   public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "Parity.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
+    System.exit(GenericTest.runFromAnnotations(args, "Parity.java", new Object() {
+    }.getClass().getEnclosingClass()).ordinal());
   }
 }
